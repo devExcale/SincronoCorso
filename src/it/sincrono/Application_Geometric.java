b02fe3f94542bd9edff0d3a99c8dd47412a2544e
Cr4zy5kyU <escaccia@live.it>
2020-01-23 10:42:36 +0100

    Application example

--- src/it/sincrono/Application_Geometric.java
+++ src/it/sincrono/Application_Geometric.java
@@ -1,14 +1,21 @@
 package it.sincrono;
 
+import it.sincrono.geometry.Pentagon;
 import it.sincrono.geometry.RegularPoligon;
+import it.sincrono.geometry.Square;
 import it.sincrono.geometry.Triangle;
 
 public class Application_Geometric {
 
 	public static void main(String[] args) {
 		
-		RegularPoligon rp = new Triangle(30D);
-		System.out.println(rp.getArea());
+		RegularPoligon rp[] = {
+			new Triangle(10D),
+			new Square(10D),
+			new Pentagon(10D)
+		};
+		for(int i=0; i<3; i++)
+			System.out.println(rp[i].getArea());
 	}
 
 }
